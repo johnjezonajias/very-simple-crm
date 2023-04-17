924301c5f8944459af9a383f53b614b52687a84a
WEBDEV-AWESOME\ACER <johnjezonajias@gmail.com>
2023-04-18 01:11:20 +0800

    AJAX data handler.

--- js/very-simple-crm-script.js
+++ js/very-simple-crm-script.js
@@ -3,32 +3,14 @@ jQuery(document).ready(function($) {
         // Prevent form submission
         event.preventDefault();
 
-        /*var form = $(this);
-        var formData = form.serialize();
-
-        console.log(formData);
-
-        $.ajax({
-            url: form.attr('action'),
-            //url: very_simple_crm_params.ajax_url,
-            type: 'post',
-            data: formData,
-            success: function(response) {
-                // Update DOM with success message
-                form.find('.customer-success').remove();
-                form.append('<p class="customer-success">Thank you for your submission!</p>');
-                form[0].reset();
-            }
-        });*/
-
         var form = $(this);
         var dataPosts = {
             'action' : 'customer_submission_ajax_handler',
-            'name'   : $('#very-simple-crm input[name="customer_name"]').val(),
-            'phone'   : $('#very-simple-crm input[name="customer_phone"]').val(),
-            'email'   : $('#very-simple-crm input[name="customer_email"]').val(),
-            'budget'   : $('#very-simple-crm input[name="customer_budget"]').val(),
-            'message'   : $('#very-simple-crm textarea#customer_message').val()
+            'customer_name'   : $('#very-simple-crm input[name="customer_name"]').val(),
+            'customer_phone'   : $('#very-simple-crm input[name="customer_phone"]').val(),
+            'customer_email'   : $('#very-simple-crm input[name="customer_email"]').val(),
+            'customer_budget'   : $('#very-simple-crm input[name="customer_budget"]').val(),
+            'customer_message'   : $('#very-simple-crm textarea#customer_message').val()
         };
 
         console.log(dataPosts);
@@ -39,9 +21,9 @@ jQuery(document).ready(function($) {
             dataType: "text",
             type : 'POST',
             success : function(data){
-                var json = $.parseJSON(data);
+                //var json = $.parseJSON(data);
                 form.find('.customer-success').remove();
-                form.append(json.message);
+                form.append('Thank you.');
                 form[0].reset();
             }
         });
